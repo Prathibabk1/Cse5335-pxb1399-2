d1f5f9c17530e8fe714ec57bdf3b3a81687f080b
unknown <prathiba.byadarahallikeshava@mavs.uta.edu>
2015-11-09 13:21:02 -0600

    edit

--- new_test.js
+++ new_test.js
@@ -1,14 +1,19 @@
 var pg = require('pg');
-
+var fs = require("fs");
+var csv = require("fast-csv");
 var connectionString =  'postgres://dodipbdfggqyte:XlvKUrEFuyTCT2Oagc4vU7jKXo@ec2-54-225-194-162.compute-1.amazonaws.com:5432/d8t7da2vk0jima';
 
 var client = new pg.Client(connectionString);
 client.connect();
 console.log ("Client connected to database");
 
-
-
-   var query = client.query("Insert into ITEMS (unitid,instnm,addr,city,stabbr,zip) values($1, $2, $3, $4, $5, $6)",[100,'uta','arlington','arling','tx','76010'],function(err, result) {
+var stream = fs.createReadStream("input1.csv");
+ 
+csv
+ .fromStream(stream, {ignoreEmpty: true})
+ .on("data", function(data){
+     console.log(data);
+     var query = client.query("Insert into ITEMS (unitid,instnm,addr,city,stabbr,zip) values($1, $2, $3, $4, $5, $6)",[parseInt(UNITID),'uta','arlington','arling','tx','76010'],function(err, result) {
       //err is the error returned from the PostgreSQL server
       //handle the error here
      if(!err)
@@ -22,6 +27,21 @@ console.log ("Client connected to database");
      console.log("done");
  });
 
+    
+    
+    
+    
+    
+    
+    
+    
+ })
+ .on("end", function(){
+     console.log("done");
+ });
+ 
+
+  
  
  
 
