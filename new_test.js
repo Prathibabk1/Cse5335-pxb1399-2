fcc2731bb539c581e3ce4690cf3fdfb721d8e3d9
unknown <prathiba.byadarahallikeshava@mavs.uta.edu>
2015-11-09 13:33:00 -0600

    edit

--- new_test.js
+++ new_test.js
@@ -12,7 +12,7 @@ var stream = fs.createReadStream("input1.csv");
 csv
  .fromStream(stream, {ignoreEmpty: true,headers : true})
  .on("data", function(data){
-     console.log(data);
+     console.log(data[0].UNITID);
      var query = client.query("Insert into ITEMS (unitid,instnm,addr,city,stabbr,zip) values($1, $2, $3, $4, $5, $6)",[parseInt(data.UNITID),'uta','arlington','arling','tx','76010'],function(err, result) {
       //err is the error returned from the PostgreSQL server
       //handle the error here
