b0549fa792fdc355f56b50550f01e929c4733283
unknown <prathiba.byadarahallikeshava@mavs.uta.edu>
2015-11-09 13:13:46 -0600

    edit

---
+++ new_test.js
@@ -0,0 +1,28 @@
+var pg = require('pg');
+
+var connectionString =  'postgres://dodipbdfggqyte:XlvKUrEFuyTCT2Oagc4vU7jKXo@ec2-54-225-194-162.compute-1.amazonaws.com:5432/d8t7da2vk0jima';
+
+var client = new pg.Client(connectionString);
+client.connect();
+console.log ("Client connected to database");
+
+
+
+   var query = client.query("Insert into ITEMS (unitid,instnm,addr,city,stabbr,zip) values($1, $2, $3, $4, $5, $6)",[100,'uta','arlington','arling','tx','76010'],function(err, result) {
+      //err is the error returned from the PostgreSQL server
+      //handle the error here
+     if(!err)
+         console.log(result);
+     else
+         console.log(err);
+     
+    });
+   query.on("end", function(){
+    client.end();
+     console.log("done");
+ });
+
+ 
+ 
+
+
