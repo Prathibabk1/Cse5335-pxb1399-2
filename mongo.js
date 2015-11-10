3c0dee6d5a16b96b455ad5b601aa99b3212001bc
unknown <prathiba.byadarahallikeshava@mavs.uta.edu>
2015-11-09 21:09:00 -0600

    edit3

--- mongo.js
+++ mongo.js
@@ -1,18 +1,10 @@
 //Lets load the mongoose module in our program
-var mongoose = require('mongoose');
+var MongoClient = require('mongodb').MongoClient;
+MongoClient.connect('mongodb://heroku_v4m7hpj0:MONGOdb123@ds053184.mongolab.com:53184/heroku_v4m7hpj0', function(err, db) {
+        if(err)
+            throw err;
+        console.log("connected to the mongoDB !");
+        
 
-// Connection URL. This is where your mongodb server is running.
-var url = 'mongodb://heroku_v4m7hpj0:MONGOdb123@ds053184.mongolab.com:53184/heroku_v4m7hpj0';
-
-//Lets connect to our database using the DB server URL.
-mongoose.connect(url);
-
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function (callback) {
-    
-    console.log("connected");
-  // yay!
 });
-db.close();
 
