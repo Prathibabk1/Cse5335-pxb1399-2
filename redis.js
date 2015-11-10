679bf9bcaba858fc90ddb4b89129d22d5b69ae4e
unknown <prathiba.byadarahallikeshava@mavs.uta.edu>
2015-11-09 22:13:30 -0600

    edit3

--- redis.js
+++ redis.js
@@ -1,7 +1,8 @@
 var redis = require('redis');
 var client = redis.createClient(16119, 'ec2-54-83-57-64.compute-1.amazonaws.com', {no_ready_check: true});
 client.auth('p2vjfv8c4pi3uudr10b0cc1tcur', function (err) {
-    if (err) then throw err;
+    if (err)
+        console.log(err);
 });
 
 client.on('connect', function() {
