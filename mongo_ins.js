b79013f6072ac260b7cc6a5412163f7471977946
unknown <prathiba.byadarahallikeshava@mavs.uta.edu>
2015-11-10 21:07:40 -0600

    edit4

--- mongo_ins.js
+++ mongo_ins.js
@@ -9,7 +9,7 @@ fs.readFile('input.json', 'utf8', function (err, data) {
 
 MongoClient.connect('mongodb://heroku_v4m7hpj0@ds053184.mongolab.com:53184/heroku_v4m7hpj0', function(err, db) {
     if(err)
-        throw err;
+        console.log( err);
     else
         console.log("connected to the mongoDB !");
 
@@ -17,12 +17,7 @@ myCollection= db.collection('test');
 console.log(myCollection);
 });
 
-myCollection.insert({"name": "doduck", "description": "learn more than everyone"}, function(err, result) {
-    if(err)
-        console.log( err);
- 
-    console.log("entry saved");
-});
+
 
 
 
