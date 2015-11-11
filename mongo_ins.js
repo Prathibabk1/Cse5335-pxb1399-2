6d9bbebd2ce855cb9bd7c4b7d61da492f20fe863
unknown <prathiba.byadarahallikeshava@mavs.uta.edu>
2015-11-10 18:56:59 -0600

    edit4

--- mongo_ins.js
+++ mongo_ins.js
@@ -6,14 +6,14 @@ fs.readFile('input.json', 'utf8', function (err, data) {
   jsonData= JSON.parse(data);
 });
 
-var db=MongoClient.connect('mongodb://heroku_v4m7hpj0@ds053184.mongolab.com:53184/heroku_v4m7hpj0', function(err, db) {
+MongoClient.connect('mongodb://heroku_v4m7hpj0@ds053184.mongolab.com:53184/heroku_v4m7hpj0', function(err, db) {
     if(err)
         throw err;
     else
         console.log("connected to the mongoDB !");
 var collection= db.collection('test');   
 for(var i=0; i<99; i++){
-var v1= {UNITID:jsonData[i].UNITID, INSTNM:jsonData[i].INSTNM, ADDR:jsonData[i].ADDR,STABBR:jsonData[i].STABBR,CITY:jsonData[i].CITY,ZIP:jsonData[i].ZIP };
+var v1= {UNITID:jsonData[i].UNITID, INSTNM:jsonData[i].INSTNM, ADDR:jsonData[i].ADDR,STABBR:jsonData[i].STABBR,CITY:jsonData[i].CITY, ZIP:jsonData[i].ZIP };
 
 collection.insert(v1,{w:1} ,function(err, input){
 if(err)
