a700faf2209434df2e84e072fbf1731feb1bcfc3
unknown <prathiba.byadarahallikeshava@mavs.uta.edu>
2015-11-10 21:15:12 -0600

    edit4

--- mongo_ins.js
+++ mongo_ins.js
@@ -7,7 +7,7 @@ fs.readFile('input.json', 'utf8', function (err, data) {
   jsonData= JSON.parse(data);
 });
 
-MongoClient.connect('mongodb://heroku_v4m7hpj0@ds053184.mongolab.com:53184/heroku_v4m7hpj0', function(err, db) {
+MongoClient.connect('mongodb://heroku_v4m7hpj0:MONGOdb123@ds053184.mongolab.com:53184/heroku_v4m7hpj0', function(err, db) {
     if(err)
         console.log( err);
     else
