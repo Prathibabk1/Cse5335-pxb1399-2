71a26a8625d7b8f1cb6ed3d58c9a27401a29b352
unknown <prathiba.byadarahallikeshava@mavs.uta.edu>
2015-11-10 21:02:49 -0600

    edit4

--- mongo_ins.js
+++ mongo_ins.js
@@ -1,18 +1,20 @@
 var MongoClient = require('mongodb').MongoClient;
 var fs = require('fs');
 var jsonData;
+var myCollection;
 fs.readFile('input.json', 'utf8', function (err, data) {
   if (err) throw err;
   jsonData= JSON.parse(data);
 });
-var myCollection;
+
 MongoClient.connect('mongodb://heroku_v4m7hpj0@ds053184.mongolab.com:53184/heroku_v4m7hpj0', function(err, db) {
     if(err)
         throw err;
     else
         console.log("connected to the mongoDB !");
 
-myCollection= db.collection('test');   
+myCollection= db.collection('test');  
+console.log(myCollection);
 });
 myCollection.insert({name: "doduck", description: "learn more than everyone"}, function(err, result) {
     if(err)
