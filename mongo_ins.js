2d08e94f1ba24a625cbaf8be8654a440d6f9df0d
unknown <prathiba.byadarahallikeshava@mavs.uta.edu>
2015-11-10 20:58:06 -0600

    edit4

--- mongo_ins.js
+++ mongo_ins.js
@@ -11,9 +11,11 @@ MongoClient.connect('mongodb://heroku_v4m7hpj0@ds053184.mongolab.com:53184/herok
         throw err;
     else
         console.log("connected to the mongoDB !");
+
 myCollection= db.collection('test');   
+});
 myCollection.insert({name: "doduck", description: "learn more than everyone"}, function(err, result) {
-    if(err)
+    if(err
         throw err;
  
     console.log("entry saved");
@@ -24,7 +26,7 @@ myCollection.insert({name: "doduck", description: "learn more than everyone"}, f
     
     
     
-});
+
 
 
 
