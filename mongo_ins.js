5988ebe59623fac6e456bf1ec3bea0f695a6d511
unknown <prathiba.byadarahallikeshava@mavs.uta.edu>
2015-11-10 21:35:17 -0600

    edit4

--- mongo_ins.js
+++ mongo_ins.js
@@ -15,16 +15,30 @@ MongoClient.connect('mongodb://admin@ds053184.mongolab.com:53184/heroku_v4m7hpj0
 
 myCollection= db.collection('test');  
 console.log(myCollection);
+    
+for(var i=0; i<99; i++){
+var v1= {UNITID:jsonData[i].UNITID, INSTNM:jsonData[i].INSTNM, ADDR:jsonData[i].ADDR,STABBR:jsonData[i].STABBR,CITY:jsonData[i].CITY, ZIP:jsonData[i].ZIP };
+myCollection.insert(v1,{w:1} ,function(err, result){
+if(err)
+console.log(err);
+else{
+console.log('inserted');
+console.log(result);
+}
+});
+}
+    
+    
+    
+});
+
+/*
 myCollection.insert({"name": "doduck", "description": "learn more than everyone"}, function(err, result) {
     if(err)
         console.log( err);
  
     console.log("entry saved");
-});
-});
-
-
-
+});*/
 
 
 
---
+++ redis_ins.js
@@ -0,0 +1,17 @@
+var redis = require('redis');
+var client = redis.createClient(16119, 'ec2-54-83-57-64.compute-1.amazonaws.com', {no_ready_check: true});
+client.auth('p2vjfv8c4pi3uudr10b0cc1tcur', function (err) {
+    if (err)
+        console.log(err);
+});
+
+client.on('connect', function() {
+    console.log('Connected to Redis');
+});
+client.set("foo", "bar", redis.print);
+client.get("foo", function (err, reply) {
+    if (err) 
+    console.log(err);
+    else
+    console.log(reply.toString());
+});
