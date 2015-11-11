fc3679212ab2374267df83ad06203385e6a527b6
unknown <prathiba.byadarahallikeshava@mavs.uta.edu>
2015-11-10 18:52:24 -0600

    edit4

--- mongo_ins.js
+++ mongo_ins.js
@@ -1,26 +1,33 @@
 var MongoClient = require('mongodb').MongoClient;
-var db=MongoClient.connect('mongodb://heroku_v4m7hpj0@ds053184.mongolab.com:53184/heroku_v4m7hpj0', function(err, db) {
-        if(err)
-            throw err;
-        else
-            console.log("connected to the mongoDB !");
-    
-});
 var fs = require('fs');
 var jsonData;
 fs.readFile('input.json', 'utf8', function (err, data) {
   if (err) throw err;
   jsonData= JSON.parse(data);
-   for (var i = 0; i < jsonData.length; ++i) {
-    console.log(" Id"+jsonData[i].UNITID);
-    console.log("Institution"+jsonData[i].INSTNM);
-    console.log("addr"+jsonData[i].ADDR);
-       console.log("addr"+jsonData[i].STABBR);
-       console.log("addr"+jsonData[i].CITY);
-       console.log("addr"+jsonData[i].ZIP);
-    
-  }
 });
+
+var db=MongoClient.connect('mongodb://heroku_v4m7hpj0@ds053184.mongolab.com:53184/heroku_v4m7hpj0', function(err, db) {
+    if(err)
+        throw err;
+    else
+        console.log("connected to the mongoDB !");
+var collection= db.collection('test');   
+for(var i=0; i<99; i++){
+var v1= {UNITID:jsonData[i].UNITID, INSTNM:jsonData[i].INSTNM, ADDR:jsonData[i].ADDR,STABBR:jsonData[i].STABBR,CITY:jsonData[i].CITY,ZIP:jsonData[i].ZIP };
+
+collection.insert(v1,{w:1} ,function(err, input){
+if(err)
+console.log('insert failed!');
+else
+console.log('inserted');
+});
+
+}
+    
+    
+    
+});
+
 
 
 
