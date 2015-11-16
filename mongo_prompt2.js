2dad9d5277aa915b331db95494291704236944a6
unknown <prathiba.byadarahallikeshava@mavs.uta.edu>
2015-11-16 14:07:07 -0600

    edit5

---
+++ mongo_prompt2.js
@@ -0,0 +1,35 @@
+var prompt = require('prompt');
+var MongoClient = require('mongodb').MongoClient;
+var myCollection;
+
+
+  prompt.start();
+
+  prompt.get(['id'], function (err, input) {
+    if (err) { console.log(err); }
+      
+      MongoClient.connect('mongodb://heroku_v4m7hpj0:4nv6cclfu1l950i645sqtvnmkb@ds053184.mongolab.com:53184/heroku_v4m7hpj0', function(err, database) {
+    if(err)
+        console.log( err);
+    else
+        console.log("connected to the mongoDB !");
+
+myCollection= database.collection('test'); 
+
+var str=input.id;
+
+myCollection.find({CITY:str}).toArray(function(err, input){
+if(err)
+console.log(err);
+input.forEach(function(result){
+console.log("mongod::   "+ result.UNITID +","+ result.INSTNM +","+ result.ADDR+","+result.CITY+","+result.STABBR+","+result.ZIP);
+});
+
+
+});
+
+
+});
+            
+      
+    });
--- redisq2.js
+++ redisq2.js
@@ -1,4 +1,5 @@
 var redis = require('redis');
+require('node-redis-streamify')(redis);
 var client = redis.createClient(16119, 'ec2-54-83-57-64.compute-1.amazonaws.com', {no_ready_check: true});
 client.auth('p2vjfv8c4pi3uudr10b0cc1tcur', function (err) {
     if (err)
@@ -9,17 +10,26 @@ client.on('connect', function() {
     console.log('Connected to Redis');
 });
 
-client.mget(1*,{"city:pheonix"}, function (err, replies) {
-    if(err)
-        console.log(err);
-    else{
-    
-    replies.forEach(function (reply, i) {
-        console.log("    " + i + ": " + reply);
-    })
-     }
-    });
-   
+
+var
+    key = '1*'
+    pattern = 'city:Phoenix',
+    count = 1,
+    hscan = redis.streamified('HSCAN'); // case insensitive 
+ 
+client.hscan(key, pattern, count)
+    .on('data', function (data) { console.log(data);})
+    .on('error', function (error) {console.log(error);})
+    .on('end', function () {client.end();});
+
+
+
+
+
+
+
+
+
 
 
 
